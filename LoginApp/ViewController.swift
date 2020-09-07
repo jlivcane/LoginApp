d405cca35297b7293f5d816eaf9a9d80dfa25230
jlivcane <kalibrik@inbox.lv>
2020-09-07 19:00:15 +0300

    Initial Commit

---
+++ LoginApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by jekaterina.livcane on 07/09/2020.
+//  Copyright © 2020 jekaterina.livcane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
